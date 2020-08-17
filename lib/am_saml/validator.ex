7146853089346f4a4bfbae9fe71cbb5e307da784
Nic Ford <nicford@nicford.co.uk>
2020-08-17 16:18:10 +0100

    Deal with potential newlines in cert

--- lib/am_saml/validator.ex
+++ lib/am_saml/validator.ex
@@ -6,12 +6,18 @@ defmodule AmSaml.Validator do
   @doc """
   Compares the certificate from the SAMLResponse with the certificate expected in env variables
   """
-  def valid_cert?(cert, cert), do: true
-  def valid_cert?(user_cert, saml_cert), do: false
+  def valid_cert?(user_cert, saml_cert) do
+    equal?(
+      String.replace(user_cert, "\n", ""),
+      String.replace(saml_cert, "\n", "")
+    )
+  end
 
   @doc """
   Compares the audience from the SAMLResponse with the audience expected in env variables
   """
-  def valid_audience?(aud, aud), do: true
-  def valid_audience?(aud, saml_aud), do: false
+  def valid_audience?(user_aud, saml_aud), do: equal?(user_aud, saml_aud)
+
+  defp equal?(a, a), do: true
+  defp equal?(_a, _not_a), do: false
 end
