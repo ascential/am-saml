ac88525b25448323335b64b773643643270183c3
Nic Ford <nicford@nicford.co.uk>
2020-10-19 18:10:23 +0100

    Make reliance on Application.get_env(:am_saml, ...) optional for most params

--- lib/am_saml.ex
+++ lib/am_saml.ex
@@ -10,6 +10,10 @@ defmodule AmSaml do
   def auth_redirect(relay_state) do
     idp_url = Application.get_env(:am_saml, :idp_url)
     issuer = Application.get_env(:am_saml, :issuer)
+    auth_redirect(relay_state, idp_url, issuer)
+  end
+
+  def auth_redirect(relay_state, idp_url, issuer) do
     ~s{#{idp_url}?SAMLRequest=#{Encoder.auth_request(issuer)}&RelayState=#{Base.url_encode64(relay_state)}}
   end
 
@@ -24,11 +28,14 @@ defmodule AmSaml do
       %{"RelayState" => "http://yoursite.com/existing_url/", "SAMLResponse" => "saml_response", "foo" => "foo", "bar" => "bar"}
 
   """
-  def auth(%{"RelayState" => relay_state, "SAMLResponse" => saml_response}, fields) do
-    %{c: cert, a: audience, i: issue_instant, d: doc} = Decoder.saml_response(saml_response)
-
+  def auth(saml_info, fields \\ []) do
     saml_cert = Application.get_env(:am_saml, :cert)
     saml_audience = Application.get_env(:am_saml, :audience)
+    auth(saml_info, saml_cert, saml_audience, fields)
+  end
+  def auth(saml_info, saml_cert, saml_audience, fields \\ []) do
+    %{"RelayState" => relay_state, "SAMLResponse" => saml_response} = saml_info
+    %{c: cert, a: audience, i: issue_instant, d: doc} = Decoder.saml_response(saml_response)
 
     if Validator.valid_cert?(cert, saml_cert) && Validator.valid_audience?(audience, saml_audience) do
       Generator.extract_fields(relay_state, issue_instant, doc, fields)
